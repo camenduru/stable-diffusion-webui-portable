9f0797b6bf3d706cdcb3bcf6157237676e38f597
camenduru <54370274+camenduru@users.noreply.github.com>
2023-04-05 13:57:05 +0300

    v2.2

--- install.bat
+++ install.bat
@@ -1,9 +1,12 @@
+@CALL "%~dp0micromamba.exe" create -n sd python=3.10 git git-lfs -c conda-forge -r "%~dp0\" -y
 @CALL "%~dp0micromamba.exe" shell init --shell=cmd.exe --prefix="%~dp0\"
 @CALL condabin\micromamba.bat activate sd
 @CALL set GDOWN_CACHE=cache\gdown
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
+@CALL pip install xformers==0.0.16
+@CALL git\bin\git.exe clone -b v2.2 https://github.com/camenduru/stable-diffusion-webui
 @CALL cd stable-diffusion-webui
-@CALL python -B webui.py --xformers
+@CALL python -B launch.py --xformers
 @CALL PAUSE
