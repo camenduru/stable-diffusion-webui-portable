e95a75735619db82bd5ea0c3133717b71a317dd2
Jeffreytsai1004 <jeffreytsai1004@gmail.com>
2023-06-27 07:28:40 +0800

    Update

--- activate.bat
+++ activate.bat
@@ -1,3 +1,2 @@
-@CALL set VENV_NAME=sd-vnev
 @CALL "%~dp0micromamba.exe" shell init --shell=cmd.exe --prefix="%~dp0\"
-start cmd /k "%~dp0condabin\micromamba.bat" activate %VENV_NAME%
+start cmd /k "%~dp0condabin\micromamba.bat" activate sd-vnev
--- run.bat
+++ run.bat
@@ -1,10 +1,9 @@
 @CALL "%~dp0micromamba.exe" shell init --shell=cmd.exe --prefix="%~dp0\"
-@CALL condabin\micromamba.bat activate sd-venv
-@REM ROOT LOCATIONS: 
+@CALL condabin\micromamba.bat activate sd-vnev
 @CALL set GDOWN_CACHE=cache\gdown
 @CALL set TORCH_HOME=cache\torch
 @CALL set HF_HOME=cache\huggingface
 @CALL set PYTHONDONTWRITEBYTECODE=1
 @CALL cd stable-diffusion-webui
-@CALL python -B webui.py --opt-sdp-attention --xformers --autolaunch --theme dark --listen
+@CALL python -B launch.py --opt-sdp-attention --xformers --autolaunch --theme dark --listen --enable-insecure-extension-access
 @CALL PAUSE
